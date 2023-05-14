655b1879d5daf65f1dea1b9f45d6c509d9cb44d7
ameen <afor1m3en@gmail.com>
2023-05-15 03:51:35 +0530

    deploy1

---
+++ src/MainContent.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import promptsData from './prompts.json';
+import ReactHtmlParser from 'react-html-parser';
+
+function MainContent() {
+    const [prompt, setPrompt] = useState('');
+  
+    const generatePrompt = () => {
+
+        const randomIndex = Math.floor(Math.random() * promptsData.length);
+        const randomPrompt = promptsData[randomIndex];
+        console.log(randomPrompt); // Log the prompt to the console
+        setPrompt(randomPrompt);
+      // Your code to generate a random prompt goes here
+
+    };
+
+    
+    return (
+        <div className="container">
+          <h1>Tell Me Something Nice</h1>
+          {/* {prompt ? ( <div className="prompt">{prompt}</div> ) : ( <p>Click the button to generate a random prompt</p> )} */}
+          {prompt && (
+        <div className="prompt">
+          <p>{ReactHtmlParser(prompt.text)}</p>
+          {prompt.author ? (<div className="author">~{prompt.author}</div>): (<p> </p>) }
+        </div>
+      )}
+      {!prompt && <p>Let me tell you how special you are</p>}
+          <button className='generate' onClick={generatePrompt}>
+            {prompt ? 'New' : 'Click this'}
+          </button>
+        </div>
+      );
+    }
+  
+  
+  export default MainContent;
