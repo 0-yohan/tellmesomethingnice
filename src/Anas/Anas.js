340899cd8a7f7cbff26321d76a2f2c8a364aa8a6
ameen <afor1m3en@gmail.com>
2024-01-28 17:15:48 +0530

    31

---
+++ src/Anas/Anas.js
@@ -0,0 +1,44 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import './Anas.css';
+
+
+const Anas = () => {
+    return(
+        <div className="page">
+            <div className="heading1" >So the day has come huh?</div>
+    <p>
+        Its been 11 years since we first met, damn thats more than a decade. I remember someone 
+        saying `if a friendship lasts more than 7 years, its a family` or something like that. 
+        Guess we can call us a family now. Lol, it happened long ago though.  
+    </p>
+    <br />
+    <p>
+        From talking about Games to listening to your Shayari, the time spent along with you was 
+        for sure is something memorable and I dont think anybody can replace that. I enjoyed it.
+        All those moments, having fun or getting scolded or just those random chatter, they were 
+        all cherishable.
+    </p>
+    <br />
+    <p>
+        Sigh, I dont know what to say, we are slowly becoming adults now. You'll be handled responsibilities, 
+        or may already been handed over. Idk what to tell, it happened really fast, we grew up really fast.
+    </p>
+    <br />
+    <p>
+        You have done really well until now, I'm proud of you. Keep on doing it, You can do it. You have it in you.
+        I mean it bro. I'm really glad that we met. I'm really fortunate to have you in my life.
+    </p>
+    <br />
+    <p>
+        Stand Proud, You're Strong. Happy 21st Birthday. May Allah make your lifespan long and prosper you 
+        with every good thing.
+    </p>
+    <br />
+    <h4><div className="sign1">Have a Great one Abdul,<br />Ameen</div></h4>
+
+        </div>
+    )
+}
+
+export default Anas;
--- src/index.js
+++ src/index.js
@@ -2,23 +2,36 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-import MainContent from './MainContent';
+import reportWebVitals from './reportWebVitals.js';
+import MainContent from './MainContent.js';
+import Anas from '../../../temp/Anas/Anas.js';
+import '../../../temp/Anas/Anas.css';
 
-function renderLetter() {
+function renderLetterJhnvi() {
   return (
-    <div className='hbd-jhnvi'>
-      <iframe src="/jhnvi.html" title="happppppy birthday" width="100%" height="100%" frameBorder="0"></iframe>
+    <div id="hbd-jhnvi">
+      <iframe src={process.env.PUBLIC_URL + '/jhnvi.html'} title="happppppy birthday" width="100%" height="100%" frameBorder="0"></iframe>
     </div>
   );
 }
+
+// function renderLetterAnas() {
+//   return (
+// <div id="hbd-anas">
+//       <iframe src={process.env.PUBLIC_URL + '/anas.html'} title="HBD Bro" width="100%" height="100%" frameBorder="0"></iframe>
+//     </div>
+//   );
+// }
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route exact path="" element={<MainContent />} />
-        <Route exact path="/jhnvi" element={<>{renderLetter()}</>} />
+        <Route exact path="/jhnvi" element={<>{renderLetterJhnvi()}</>} />
+        <Route exact path="/anas" element={<Anas />} />
+
       </Routes>
     </Router>
   </React.StrictMode>,
